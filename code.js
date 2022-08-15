5439b60ba29cbd42779cb0d7e3b223997e3c90f9
samjoe3 <tylerstanco@hotmail.com>
2022-08-15 11:26:39 -0400

    working on css grid, specifically adding DOM JS shit to try to get a fuckking dynamic grid working, making progress, not finished.

--- code.js
+++ code.js
@@ -14,14 +14,17 @@ function defaultGrid() {
 function generateGrid(num1) {
     while (grid.hasChildNodes()) grid.removeChild(grid.firstChild);
 
-    num1 = prompt('Enter a number between 1 and 100.');
-
-    if (num1 >= 1 && num1 <= 1000) {
+    num1 = prompt('Enter a number between 1 and 10,000.');
+    let columnNumber = document.querySelector('.grid');
+    if (num1 >= 1 && num1 <= 10000) {
+        //columnNumber.style.gridTemplateColumns = `repeat(${num1}, ${720/num1}px)`;
+        columnNumber.style.gridTemplateRows = `repeat(${num1}), ${720/num1}px)`;
         for (let i = 0; i < num1; i++) {
             gridBox = document.createElement('gridbox');
             //gridBox.innerHTML = `T${i+1} `;
             grid.appendChild(gridBox);
             gridBox.classList.add('gridbox');
+            gridBox.style.maxHeight = `${720/num1}px`;
         }
     }
     else alert("Your entry is not a number between 1 and 100, please try again.");
