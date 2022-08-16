3803f737e4ed9e2ce0ba816f71c636c8eb832c57
samjoe3 <tylerstanco@hotmail.com>
2022-08-16 07:24:15 -0400

    completed working grid, need to change code to take numxnum instead of the num^2.

--- code.js
+++ code.js
@@ -16,15 +16,15 @@ function generateGrid(num1) {
 
     num1 = prompt('Enter a number between 1 and 10,000.');
     let columnNumber = document.querySelector('.grid');
+    let num2 = (720/Math.sqrt(num1));
     if (num1 >= 1 && num1 <= 10000) {
-        //columnNumber.style.gridTemplateColumns = `repeat(${num1}, ${720/num1}px)`;
-        columnNumber.style.gridTemplateRows = `repeat(${num1}), ${720/num1}px)`;
+        columnNumber.style.gridTemplateColumns = `repeat(${Math.sqrt(num1)}, ${num2}px)`;
+        columnNumber.style.gridTemplateRows = `repeat(${Math.sqrt(num1)}, ${num2}px)`;
+
         for (let i = 0; i < num1; i++) {
             gridBox = document.createElement('gridbox');
-            //gridBox.innerHTML = `T${i+1} `;
             grid.appendChild(gridBox);
             gridBox.classList.add('gridbox');
-            gridBox.style.maxHeight = `${720/num1}px`;
         }
     }
     else alert("Your entry is not a number between 1 and 100, please try again.");
